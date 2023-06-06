c94fb733c711d9ed1543636f1b4f0ccb43bdf700
Wojciech <84650124+wojciechignasiak@users.noreply.github.com>
2023-06-06 17:47:07 +0200

    name changes, jwt plugin instalation, added exp claims to verify

--- fastapi-app/app/kong/create_jwt_credential_for_consumer_using_id.py
+++ fastapi-app/app/kong/create_jwt_credential_for_consumer_using_id.py
@@ -3,7 +3,7 @@ from fastapi import HTTPException
 from app.models.kong_jwt import KongJWTCredentials
 
 async def create_jwt_credential_for_consumer_using_id(kong_consumer_id: str):
-    """Function to create jwt credential for a consumer.
+    """Function that creates jwt credential for a consumer.
 
     This function will create a new jwt credential for a consumer.
 
@@ -12,6 +12,13 @@ async def create_jwt_credential_for_consumer_using_id(kong_consumer_id: str):
 
     Returns:
         created_jwt_credential (KongJWTCredentials): The created jwt credential.
+
+    Raises:
+        HTTPException: 500 if fail.
+        
+    Example:
+        >>> create_jwt_credential_for_consumer_using_id(kong_consumer_id="4f596ce5-f799-4bc7-9c54-cfc90c3eff42")
+        json
     """
     try:
         async with httpx.AsyncClient() as client:
@@ -19,5 +26,7 @@ async def create_jwt_credential_for_consumer_using_id(kong_consumer_id: str):
         if response.is_success:
             created_jwt_credential: KongJWTCredentials = KongJWTCredentials(**response.json())
             return created_jwt_credential
+        else:
+            raise HTTPException(status_code=500, detail=f"create_jwt_credential_for_consumer_using_id: {response.text}")
     except httpx.HTTPError as e:
         raise HTTPException(status_code=500, detail=f"create_jwt_credential_for_consumer_using_id: {e}")
